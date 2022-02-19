3a0ad84b5f359bc80e4523382017a929a12ec1a5
ramevans <72097409+ramevans@users.noreply.github.com>
2022-02-19 10:54:25 -0500

    incorporated temperature device

--- device_module.py
+++ device_module.py
@@ -10,6 +10,8 @@ except:
     print("JSON error")
     exit()
     #print (data)
+# This variable access the Temperature value from the JSON File
+Therometer_info = data['Devices']['Therometer']['Temperature']
 
 # this checks if the these fields are the proper structure type
 def check_type(all_device):
@@ -23,7 +25,22 @@ def check_type(all_device):
         if not isinstance(device['User'],str):
             return "Incorrect User (" + str(device_info)+")"
     return 0
+# this checks the Therometer Device values and indicate to the user if temperature is above or below normal temp.
+def temperature_info(Therometer):
+    if not isinstance(Therometer, (int,float)):
+        return "Please Enter a Proper Value"
+    if Therometer < 97:
+        #insert Alert Module function here:
+        return ('Your Body temperature is Below Normal Temperature')
+    elif Therometer > 99:
+        #insert Alert Module function here:
+        return ('Your Body temperature is Above Normal Temperature')
+    return 0
 
+# Error Codes
 error_code=check_type(data['Devices'])
 if error_code!=0:
     print(error_code, "1")
+error_code = temperature_info(Therometer_info)
+if error_code!=0:
+    print(error_code, "2")
