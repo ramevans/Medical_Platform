a0529b0a5cb58c6e54f5978152bde60541d4779f
ramevans <72097409+ramevans@users.noreply.github.com>
2022-02-26 11:11:22 -0500

    Creates Initial Database File in Directory
    this creates the initial database file in the directory by using the "db.create_all()" function after I remove the function so it doesn't continuously creating a new database

--- restful.py
+++ restful.py
@@ -1,8 +1,24 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db =SQLAlchemy(app)
+
+class DevicesModel(db.Model):
+    device_name = db.Column(db.String(), primary_key=True, nullable=False)
+    device_MAC = db.Column(db.String(),nullable=False, primary_key= True)
+    user= db.Column(db.String(),nullable=False)
+    Temperature = db.Column(db.Integer,primary_key= True, nullable=False)
+
+    def __repr__(self):
+        return f"Devices(Device Name={device_name}, Device MAC Address={device_MAC}, User={user}, Temperature= {Temperature})"
+
+db.create_all()
+
 
 device_put_args = reqparse.RequestParser()
 device_put_args.add_argument("Device Name", type=str, help="Name of the Device", required =True)
@@ -13,11 +29,11 @@ device_put_args.add_argument("Temperature", type=int, help="Name of the Device",
 therometer = {}
 
 def abort_nonexist(device_info):
-    if device_info not in Devices:
+    if device_info not in therometer:
         abort(404, message= "No Device Infomation Provided")
 
 def abort_exist(device_info):
-    if device_info in Devices:
+    if device_info in therometer:
         abort(409, message= "Device Already Exist ")
 class Devices(Resource):
     def get(self, device_info):
@@ -38,3 +54,4 @@ api.add_resource(Devices, "/Devices/<string:device_info>")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
